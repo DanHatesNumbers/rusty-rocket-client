0e9a855bb7d765ea746af4719534f6dd38c1c903
Daniel Murphy <danhatesnumbers@gmail.com>
2018-05-28 20:23:47 +0100

    Added client that does two simultaneous posts. Doesn't handle failed requests properly

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ extern crate hyper;
 extern crate tokio_core;
 
 use std::io::{self, Write};
-use std::str::FromStr;
-use futures::{Future, Stream};
-use hyper::{Client, Uri};
+use std::str::{self, FromStr};
+use futures::{Future, future, Stream};
+use hyper::{Client, Method, Request, Uri};
 use hyper::error::{Error, UriError};
+use hyper::header::{ContentLength, ContentType};
 use tokio_core::reactor::Core;
 
 #[derive(Fail, Debug)]
@@ -47,16 +48,45 @@ fn main() -> Result<(), AppError> {
 
     let uri = Uri::from_str("http://192.168.99.100:8080/people")?;
 
-    let work = client.get(uri).and_then(|res| {
-        println!("Response: {}", res.status());
+    let dan = r#"{"id": 0, "first_name": "Dan", "last_name": "Murphy", "age": 26, "profession": "Quality Engineer", "salary": 45000}"#;
+    let graham = r#"{"id": 0, "first_name": "Graham", "last_name": "Sutherland", "age": 30, "profession": "Hacker", "salary": 55000}"#;
 
-        res.body().for_each(|chunk| {
-            io::stdout()
-                .write_all(&chunk)
-                .map_err(From::from)
+    let mut dan_req = Request::new(Method::Post, uri.clone());
+    dan_req.headers_mut().set(ContentType::json());
+    dan_req.headers_mut().set(ContentLength(dan.len() as u64));
+    dan_req.set_body(dan);
+    let dan_post = client.request(dan_req);
+    
+    let mut graham_req = Request::new(Method::Post, uri.clone());
+    graham_req.headers_mut().set(ContentType::json());
+    graham_req.headers_mut().set(ContentLength(graham.len() as u64));
+    graham_req.set_body(graham);
+
+    let graham_post = client.request(graham_req);
+
+    let work = future::join_all(vec![dan_post, graham_post])
+        .and_then(|results| {
+            future::ok(results.into_iter()
+                .map(|result| {
+                    println!("Status: {}", result.status());
+
+                    result.body().concat2()
+                })
+                .collect::<Vec<_>>()
+            )
+        });
+
+    let results: Vec<futures::stream::Concat2<hyper::Body>> = core.run(work).unwrap();
+
+    let parsed_results = results.into_iter()
+        .map(|result| {
+            str::from_utf8(&result.wait().unwrap()).unwrap().to_string()
         })
-    });
+        .collect::<Vec<_>>();
 
-    core.run(work)
-        .map_err(From::from)
+    for body in parsed_results {
+        println!("{}", body);
+    }
+
+    Ok(())
 }
